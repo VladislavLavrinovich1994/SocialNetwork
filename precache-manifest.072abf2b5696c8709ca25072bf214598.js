67965867be4d1eb3527ac309c93c9906fc72523f
VladislavLavrinovich1994 <markvimand@gmail.com>
2019-11-05 15:24:33 +0300

    Updates

---
+++ precache-manifest.072abf2b5696c8709ca25072bf214598.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f2460644f4b5b2aba307e1e6e83a6b",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "ffb592290b0fb5c8ae5a",
+    "url": "/SocialNetwork/static/css/main.5e1e979c.chunk.css"
+  },
+  {
+    "revision": "c4b9050a73bc2e024e9b",
+    "url": "/SocialNetwork/static/js/2.d3300135.chunk.js"
+  },
+  {
+    "revision": "ffb592290b0fb5c8ae5a",
+    "url": "/SocialNetwork/static/js/main.94a64ae4.chunk.js"
+  },
+  {
+    "revision": "636a0223770ff407f4fe",
+    "url": "/SocialNetwork/static/js/runtime~main.82579fc5.js"
+  }
+]);
